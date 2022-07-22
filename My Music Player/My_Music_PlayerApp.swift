289401be79037d05040b167daeddd32591e8b5c0
Kostya <epoha.io@icloud.com>
2022-07-22 23:46:51 +0300

    Initial Commit

---
+++ My Music Player/My_Music_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_Music_PlayerApp.swift
+//  My Music Player
+//
+//  Created by Kostiantyn Nevinchanyi on 7/22/22.
+//
+
+import SwiftUI
+
+@main
+struct My_Music_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
